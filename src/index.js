f74c2a039930c1ea05e1c500f342cf89bd2f8690
redbuttonking <103170787+redbuttonking@users.noreply.github.com>
2024-05-03 15:32:02 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  // <App/>이 감싸져있다면 개발모드에서 (계발 단계에서 오류잡으려고) 두번씩 렌더링이 됨
-  // </React.StrictMode>
-
-  <App />
+  // {/* // <App/>이 감싸져있다면 개발모드에서 (계발 단계에서 오류잡으려고) 두번씩 렌더링이 됨 */}
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  // <BrowserRouter basename={process.env.PUBLIC_URL}>
 );
